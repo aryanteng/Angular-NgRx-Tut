b2585dd8f6a96f036aee9063e79c8caf0b32fbe3
aryanteng <aryanteng892@gmail.com>
2023-08-30 18:38:30 +0530

    routing setup

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,18 +4,22 @@ import { CustomerComponent } from './customer/customer.component';
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerEditComponent } from './customer-edit/customer-edit.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomerComponent,
+  },
+];
 
 @NgModule({
   declarations: [
     CustomerComponent,
     CustomerAddComponent,
     CustomerEditComponent,
-    CustomerListComponent
+    CustomerListComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class CustomersModule { }
+export class CustomersModule {}
